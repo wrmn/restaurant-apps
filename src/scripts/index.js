d5c1e8ffd90d277366fa17545a4d172874550f46
warmoa <moawarman@gmail.com>
2020-09-09 10:26:24 +0700

    tab order

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,11 +2,23 @@ import 'regenerator-runtime'; /* for async await transpile */
 import './../styles/styles.scss';
 
 (function () {
+    const menuTrigger = document.querySelector('.burger-container');
     const headerMobile = document.querySelector('.header-mobile');
 
-    headerMobile.onclick = function () {
+    menuTrigger.onclick = function () {
         headerMobile.classList.toggle('menu-opened');
+        (headerMobile.classList.contains('menu-opened')) ? tabindex(0): tabindex(-1);
     }
 }());
+
+const tabindex = (index) => {
+    const items = document.querySelectorAll('.menu-name');
+    // for (const item in items) {
+    //     console.log(item);
+    // }
+    items.forEach((item) => {
+        item.tabIndex = index;
+    })
+}
 
 console.log('Hello Coders! :)');
